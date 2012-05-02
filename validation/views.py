51375862e3cfe04ddcab1874fa851872a64019dd
sbeard <sbeard@cs.princeton.edu>
2012-05-02 18:08:40 -0400

    Start implementing the changes to print categories

--- validation/views.py
+++ validation/views.py
@@ -1,7 +1,31 @@
 # Create your views here.
 from data_import.views import read_dataset
-from ratings.nmf import run_nmf_mult_k
+from ratings.nmf import run_nmf_mult_k,get_p_q_best
 from ratings.populate import pop_test_user_bus_data, generate_nmf_test
+
+Steps = 20000
+Alpha = 0.0035
+
+def find_categories_best_k(k):
+    P, Q = get_p_q_best(k, Steps, Alpha)
+    zipQ = zip(*Q)
+    for l in zipQ:
+      maxVal = max(l)
+      cutOff = 0.9 * maxVal
+      print "Cutoff is: " + str(cutOff)
+
+      relevantBus = []
+      for i in range(0, len(l)):
+        if l[i] > cutOff:
+          # this business is relevant to this latent variable
+          # save the id (index is id-1) to use in db look up later
+          relevantBus.append(i+1) 
+      
+      # For this latent variable, we now have all businesses IDs,
+      # print out all of the labels associated with these businesses
+
+
+         
 
 
 def validate_production_data():
@@ -9,8 +33,6 @@ def validate_production_data():
     #read_dataset()
     # K = [12,13,14,15,16,17,18]
     K = [22,24]
-    Steps = 20000
-    Alpha = 0.0035
     run_nmf_mult_k(K,Steps,Alpha)
 
 def simple_validate():
@@ -18,6 +40,4 @@ def simple_validate():
     generate_nmf_test(numFactors=6, density=.3)
     print("here?")
     K = [1,3,6,9,12]
-    Steps = 20000
-    Alpha = 0.0035
     run_nmf_mult_k(K,Steps,Alpha)
