e3750707a3b78c2efaad9ccb62eb3cbaeb3f500f
sbeard <sbeard@cs.princeton.edu>
2012-05-03 14:36:16 -0400

    Update the stuff to print categories

--- validation/views.py
+++ validation/views.py
@@ -1,29 +1,44 @@
 # Create your views here.
 from data_import.views import read_dataset
+from django.conf import settings
 from ratings.nmf import run_nmf_mult_k,get_p_q_best
 from ratings.populate import pop_test_user_bus_data, generate_nmf_test
+from ratings.models import Business
 
-Steps = 20000
+Steps = 200
 Alpha = 0.0035
 
 def find_categories_best_k(k):
     P, Q = get_p_q_best(k, Steps, Alpha)
     zipQ = zip(*Q)
-    for l in zipQ:
+    latentNum = 0
+    for l in zipQ: #{
       maxVal = max(l)
-      cutOff = 0.9 * maxVal
-      print "Cutoff is: " + str(cutOff)
+      cutOff = 0.5 * maxVal
+      print "  Cutoff is: " + str(cutOff)
 
       relevantBus = []
-      for i in range(0, len(l)):
+      for i in xrange(0, len(l)):
         if l[i] > cutOff:
           # this business is relevant to this latent variable
           # save the id (index is id-1) to use in db look up later
           relevantBus.append(i+1) 
       
+      print "    " + str(len(relevantBus)) + " businesses past cutoff"
       # For this latent variable, we now have all businesses IDs,
       # print out all of the labels associated with these businesses
+      fp = open(settings.RESULTS_DIR + "latent_" + str(latentNum), 'w')
+      buses = Business.objects.filter(pk__in=relevantBus)
+      for b in buses:
+        keywords = b.keywords.all()
+        print "      " + str(len(keywords)) + " keywords for business"
+        for k in keywords:
+          fp.write(str(k) + "\n")
+        fp.write("\n")
 
+      fp.close()
+      latentNum = latentNum + 1
+    #}
 
          
 
