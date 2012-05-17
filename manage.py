9ec18b52032586d7e983ed5606245f00670c4431
zouf <mattzouf@gmail.com>
2012-05-17 15:11:47 -0400

    need init

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
-    
+    sys.path.append("/app/rateout")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     from django.core.management import execute_from_command_line
 
