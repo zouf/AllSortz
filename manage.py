7953cb5a88782f6431c2592fad8b5f1e57287eac
zouf <mattzouf@gmail.com>
2012-05-17 12:57:45 -0400

    manage'

--- manage.py
+++ manage.py
@@ -2,9 +2,11 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rateout.settings")
-
+    print("zouf")
+    sys.path.append('.')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
