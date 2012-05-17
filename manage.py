048d8a23ed96e274e93e0196566815211f7bff89
zouf <mattzouf@gmail.com>
2012-05-17 15:21:28 -0400

    proper files

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ import sys
 
 
 if __name__ == "__main__":
-    sys.path.append("/app/rateout")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    sys.path.append('/Users/zouf/Sites/rateout-2/rateout')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rateout.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
