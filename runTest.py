a23d5023939c83337557dbea43f7a4569deab4bc
zouf <mattzouf@gmail.com>
2012-05-17 11:48:55 -0400

    Change name

--- runTest.py
+++ runTest.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nightout.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rateout.settings")
 
     from django.core.management import execute_from_command_line
     from validation.views import validate_production_data
