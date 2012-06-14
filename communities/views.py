baaca9bed669773fd572e9891cb505f70f407ee4
zouf <mattzouf@gmail.com>
2012-06-14 11:07:04 -0400

    Fix default adder for communitiest

--- communities/views.py
+++ communities/views.py
@@ -1,15 +1,26 @@
 # Create your views here.
 from communities.models import UserMembership, Community
+import logging
 
+
+logger = logging.getLogger(__name__)
+
+def get_default():
+    try:
+        c = Community.objects.get(name="Princeton")
+    except:
+        logger.debug("Creating a default community of Princeton")
+        c = Community(name="Princeton", descr="Default Community of Princeton", city="Princeton")
+        c.save()
+    return c
 
 def get_community(user):
     if user.is_authenticated():
         try:
             membership = UserMembership.objects.get(user=user,default=True)
-            community = membership.community
+            return membership.community
         except:
         #TODO change to be based on browser location
-            community = Community.objects.get(name="Princeton")
-        return community
-    return Community.objects.get(name="Princeton")
+            return get_default()
+    return get_default()
         
