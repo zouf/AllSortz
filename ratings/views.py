011193bea7a5885436d0474dbdfe1c0ad32334bd
zouf <mattzouf@gmail.com>
2012-07-16 10:23:52 -0400

    styling updates

--- ratings/views.py
+++ ratings/views.py
@@ -1,20 +1,9 @@
-from allsortz.feed import get_user_recent_activity
-from allsortz.views import get_default_bus_context, get_default_tag_context, \
-    get_default_blank_context, bus_details
-from communities.models import UserMembership
-from django.contrib.auth import logout
-from django.contrib.auth.models import User
+from allsortz.views import get_default_blank_context
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.template import RequestContext
-from photos.views import get_photo_web_url, get_user_profile_pic
-from ratings.favorite import get_user_favorites
-from ratings.models import Business, PageRelationship
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 from tags.form import TagForm
 from tags.models import Tag
-from usertraits.views import get_user_traits
-from wiki.forms import PageForm
-from wiki.models import Page
 import logging
 
 
